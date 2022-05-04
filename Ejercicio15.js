bbc2a48aea1c3be4229ccd9270ffc2bdccea0bdd
Yon Aristizabal <ygaristizabal81046@umanizales.edu.co>
2022-05-04 18:08:33 -0500

    Ejercicio 15

---
+++ Ejercicio15.js
@@ -0,0 +1,8 @@
+var text= prompt("Hola, ingrese una frase");
+var text1= prompt("Hola, ingrese otra frase");
+
+if (text.length < text1.length){
+    console.log(true);
+} else {
+    console.log(false);
+}
